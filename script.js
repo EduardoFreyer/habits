6578064a0448d2d97afed5dd496f0e882fe8e935
EduFreyer <mastergamesbr5@gmail.com>
2023-01-21 02:27:50 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const form = document.querySelector("form")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
+
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso")
+    return
+  }
+
+  alert("Adicionado com sucesso ✅")
+  nlwSetup.addDay(today)
+}
+
+function save() {
+  localStorage.setItem("habitsSave", JSON.stringify(nlwSetup.data))
+}
+
+/*
+const data = {
+  run: ["01-01", "01-02", "01-03"],
+}
+*/
+
+const data = JSON.parse(localStorage.getItem("habitsSave")) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
